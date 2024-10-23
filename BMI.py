392967a7aef392974777d3990fdef5f6b6ba4483
fatemeh <fatemehrouhafzaei@gmail.com>
2024-10-23 21:49:12 +0330

    def age max

--- BMI.py
+++ BMI.py
@@ -4,8 +4,23 @@ def calculate_BMI () :
     height_cm = int(height_int.get())
     height_m = height_cm/100
     weight = int(weight_int.get())
-    BMI = weight/height_m**2
-    return(round(BMI , 2))
+    # BMI = weight/height_m**2
+    return(round(weight/height_m**2 , 2))
+def age_max_BMI():
+    BMI = calculate_BMI
+    if BMI < 18.5 :
+        print("underweight")
+    elif 18.5 < BMI < 24.5 :
+        print("healthy weight")
+    elif 24.5 < BMI < 29.9 :
+        print("overweight")
+    else :
+        print("fat")
+def BMI_show () :
+    app =ttk.Window("BMI","solar", resizable=(False, False))
+    BMI_answer = ttk.Label(app, text="BMI_answer")
+    BMI_answer.grid(columnspan=2, row=0, sticky=ttk.N, padx=20, pady=5)
+
 window = ttk.Window("BMI ","solar", resizable=(False, False))
 BMI_lbl = ttk.Label(window, text="BMI_Calculation")
 BMI_lbl.grid(columnspan=2, row=0, sticky=ttk.N, padx=20, pady=5)
@@ -22,8 +37,9 @@ weight_int.grid(column=1, row=2, sticky=ttk.W, padx=10, pady=5)
 #age-------------------
 age_btn= ttk.Checkbutton(window, text="age > 19 ", bootstyle="secondary", command=calculate_BMI)
 age_btn.grid(column=0, row=3, sticky=ttk.EW, padx=10, pady=5)
-age_btn_1 = ttk.Checkbutton(window, text='age < 19 ', bootstyle="secondary", )
-age_btn_1.grid(column=0, row=4, sticky=ttk.EW, padx=10, pady=5)
+# age_btn_1 = ttk.Checkbutton(window, text='age < 19 ', bootstyle="secondary", )
+# age_btn_1.grid(column=0, row=4, sticky=ttk.EW, padx=10, pady=5)
 #apply-----------------
-submit_btn = ttk.Button(window, bootstyle="danger" , text="apply")
+submit_btn = ttk.Button(window, bootstyle="danger" , text="apply", command=age_max_BMI)
 submit_btn.grid(columnspan=2, row=6, sticky=ttk.EW, padx=10, pady=5)
+window.mainloop()
